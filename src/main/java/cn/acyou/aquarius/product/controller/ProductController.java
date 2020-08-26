ce2027f92b42ed52bc48a59ebd7aa887bcd117b5
981545521@qq.com <Fy52007825>
2020-08-26 10:31:24 +0800

    增加方法

--- src/main/java/cn/acyou/aquarius/product/controller/ProductController.java
+++ src/main/java/cn/acyou/aquarius/product/controller/ProductController.java
@@ -3,13 +3,12 @@ package cn.acyou.aquarius.product.controller;
 import cn.acyou.aquarius.product.common.Result;
 import cn.acyou.aquarius.product.entity.Product;
 import cn.acyou.aquarius.product.mapper.ProductMapper;
-import cn.acyou.aquarius.product.mapper.ProductMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,5 +25,15 @@ public class ProductController {
     public Result<?> list() {
         List<Product> products = productMapper.selectAll();
         return Result.success(products);
+    }
+    @GetMapping(value = "/outStock")
+    public List<String> outStock(){
+        List<String> rs = new ArrayList<>();
+        Product product = productMapper.selectByPrimaryKey(1);
+        product.setStockNumber(product.getStockNumber() - 1);
+        productMapper.updateByPrimaryKeySelective(product);
+        System.out.println("扣减库存");
+        rs.add("扣减库存成功！");
+        return rs;
     }
 }
